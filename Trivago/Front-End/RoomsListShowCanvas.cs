75722dd825528de7c8d314d2ce4491aee64f20da
islammohsen <islammohsen2@hotmail.com>
2019-04-29 00:14:53 +0200

    updated roomlistshow canvas

--- Trivago/Front-End/RoomsListShowCanvas.cs
+++ Trivago/Front-End/RoomsListShowCanvas.cs
@@ -10,6 +10,7 @@ namespace Trivago.Front_End
     {
         private static RoomsListShowCanvas roomsListShowCanvas;
         private List<Room> rooms;
+        private List<MealPlan> mealPlans;
 
         private RoomsListShowCanvas(Canvas canvas) : base(canvas)
         {
@@ -26,9 +27,11 @@ namespace Trivago.Front_End
 
         public override void Initialize()
         {
+            mealPlans = new List<MealPlan>(new MealPlan[rooms.Count]);
+
             int boxSpacing = 50;
             double cardWidth = canvas.Width - 0.2 * canvas.Width;
-            double cardHeight = 0.5 * canvas.Height;
+            double cardHeight = 0.7 * canvas.Height;
 
             ScrollViewer roomScrollViewer = new ScrollViewer
             {
@@ -38,7 +41,7 @@ namespace Trivago.Front_End
 
             StackPanel roomCardStackPanel = new StackPanel();
             roomScrollViewer.Content = roomCardStackPanel;
-            
+
             for (int i = 0; i < rooms.Count; i++)
             {
                 Room room = rooms[i];
@@ -49,33 +52,42 @@ namespace Trivago.Front_End
                     BorderBrush = new SolidColorBrush(Color.FromRgb(0, 0, 0)),
                     BorderThickness = new Thickness(3),
                     Width = cardWidth,
-                    Height = cardHeight,
                     Margin = new Thickness(0.1 * canvas.Width, boxSpacing, 0.1 * canvas.Width, 0)
                 };
                 Grid roomGrid = new Grid
                 {
                     Width = cardWidth,
-                    Height = cardHeight,
+
+                    ColumnDefinitions =
+                    {
+                        new ColumnDefinition { Width = new GridLength(0.4 * cardWidth) },
+                        new ColumnDefinition { Width = new GridLength(0.6 * cardWidth) }
+                    },
+                    RowDefinitions =
+                    {
+                        new RowDefinition { Height = GridLength.Auto },
+                        new RowDefinition { Height = GridLength.Auto }
+                    }
                 };
-                roomGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(0.4 * cardWidth) } );
-                roomGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(0.6 * cardWidth) });
                 cardBorder.Child = roomGrid;
                 roomCardStackPanel.Children.Add(cardBorder);
 
                 //set card image
                 Image roomImage = room.image.GetImage();
-                roomImage.MaxWidth = 0.4 * cardWidth;
-                roomImage.MaxHeight = 0.8 * cardHeight;
+                roomImage.MaxWidth = 0.35 * cardWidth;
+                roomImage.MaxHeight = 0.85 * cardHeight;
                 Grid.SetColumn(roomImage, 0);
+                Grid.SetRow(roomImage, 0);
                 roomGrid.Children.Add(roomImage);
                 
                 //set roomdatastackpanel
                 StackPanel roomDataStackPanel = new StackPanel
                 {
                     Width = 0.6 * cardWidth,
-                    Height = cardHeight
+                    Height = 0.85 * cardHeight,
                 };
                 Grid.SetColumn(roomDataStackPanel, 1);
+                Grid.SetRow(roomDataStackPanel, 0);
                 roomGrid.Children.Add(roomDataStackPanel);
 
                 //set room type
@@ -119,24 +131,62 @@ namespace Trivago.Front_End
                 Label locationLabel = new Label
                 {
                     Content = "Loaction: " + room.hotel.location.city + ", " + room.hotel.location.country,
-                    FontSize = 22
+                    FontSize = 22,
                 };
                 Grid.SetColumn(locationLabel, 0);
                 grid.Children.Add(locationLabel);
 
                 Button viewMoreButton = new Button
                 {
-                    Content = "View More",
+                    Content = "Reserve",
                     Width = cardWidth * 0.1,
                     Height = cardHeight * 0.1,
-                    Margin = new Thickness(0, 0.05 * cardHeight, 0.05 * cardWidth, 0),
                     HorizontalAlignment = HorizontalAlignment.Center
                 };
                 Grid.SetColumn(viewMoreButton, 1);
                 grid.Children.Add(viewMoreButton);
-
                 roomDataStackPanel.Children.Add(grid);
+
+                Expander viewMoreExpander = new Expander
+                {
+                    Width = canvas.Width,
+                    Margin = new Thickness(0, 0, 0, 0.05 * cardHeight)
+                };
+                Grid.SetColumnSpan(viewMoreExpander, 2);
+                Grid.SetRow(viewMoreExpander, 1);
+                roomGrid.Children.Add(viewMoreExpander);
+
+                TabControl MoreDetailsTabs = new TabControl();
+                viewMoreExpander.Content = MoreDetailsTabs;
+
+                TabItem MealsTab = new TabItem { Header = "Meals" };
+                StackPanel MealsPanel = new StackPanel();
+                MealsTab.Content = MealsPanel;
+                MoreDetailsTabs.Items.Add(MealsTab);
+
+                for (int j = 0; j < room.hotel.mealPlans.Count; j++)
+                {
+                    MealPlan mealPlan = room.hotel.mealPlans[j];
+                    RadioButton mealPlanRadioButton = new RadioButton
+                    {
+                        GroupName = i.ToString(),
+                        Content = mealPlan,
+                        FontSize = 22,
+                        Margin = new Thickness(0, 0.025 * cardHeight, 0, 0)
+                    };
+                    mealPlanRadioButton.Checked += mealRadioButtonChecked;
+                    if (j == 0)
+                        mealPlanRadioButton.IsChecked = true;
+                    MealsPanel.Children.Add(mealPlanRadioButton);
+                }
             }
+        }
+
+        private void mealRadioButtonChecked(object sender, RoutedEventArgs args)
+        {
+            RadioButton mealRadioButton = (RadioButton)sender;
+            int idx = int.Parse(mealRadioButton.GroupName);
+            mealPlans[idx] = (MealPlan)mealRadioButton.Content;
         }
     }
 }
