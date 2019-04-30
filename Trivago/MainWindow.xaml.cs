360a5eebd2b72919e21dd6df81c024e48c45e0dd
islammohsen <islammohsen2@hotmail.com>
2019-05-01 01:32:15 +0200

    Added active user and modified reserve popup

--- Trivago/MainWindow.xaml.cs
+++ Trivago/MainWindow.xaml.cs
@@ -29,6 +29,7 @@ namespace Trivago
         private const double NavigationCanvasHeight = 100;
         public CustomCanvas CurrentCanvas;
         public CustomCanvas currentNavigationCanvas;
+        public User ActiveUser;
 
         public MainWindow()
         {
@@ -149,6 +150,7 @@ namespace Trivago
             }
             Front_End.NavigationCanvas.GetInstance(NavigationCanvas).Hide();
             InitializeLoggedinNavigationCanvas(user);
+            ActiveUser = user;
 
             CurrentCanvas.Hide();
             InitializeHomeCanvas();
@@ -202,6 +204,7 @@ namespace Trivago
             }
             Front_End.NavigationCanvas.GetInstance(NavigationCanvas).Hide();
             InitializeLoggedinNavigationCanvas(user);
+            ActiveUser = user;
 
             CurrentCanvas.Hide();
             InitializeHomeCanvas();
@@ -216,6 +219,11 @@ namespace Trivago
 
         public void ReserveButton_Click(object sender, RoutedEventArgs args)
         {
+            if(ActiveUser == null)
+            {
+                MessageBox.Show("Please Login");
+                return;
+            }
             Button reserveButton = (Button)sender;
             int index = (int)reserveButton.Tag;
             RoomsListShowCanvas roomListShowCanvas = (RoomsListShowCanvas)CurrentCanvas;
